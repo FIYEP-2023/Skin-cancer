77768aee314106b11667425408075633ab5c02cb
Pedro Prazeres <pedronprazeres@gmail.com>
2023-04-28 09:14:50 +0200

    compactness updated (needs review)

--- feature_extraction/feature_extractor.py
+++ feature_extraction/feature_extractor.py
@@ -4,8 +4,21 @@ class FeatureExtractor:
         return 0 # return the value of the feature
 
     def compactness(self, img, mask):
-        print(">w<")
-        return 0 # return the value of the feature
+        ''' move these imports upwards if used in other functions too (?) '''
+        from scipy import ndimage # updated morphology in newest scipy(?)
+        import numpy as np # needs full import for np.delete(?)
+
+        if img.shape[2] == 4: # if img has 4th dim (alpha)
+            img = np.delete(img, 3, 2) # remove alpha
+
+        brush = ndimage.generate_binary_structure(3, 1) # create 3 dim erosion brush
+        eroded = ndimage.binary_erosion(mask, brush) # eroded mask
+        p = np.sum(mask - eroded) # find perimeter
+
+        area = np.sum(mask) # lesion area
+
+        c = (p**2)/(4*area)
+        return c
     
     def color(self, img, mask):
         print("owo")
