9df090d1551ded9e4b5e5c6bdb7b6e6a5b8457b6
Alexander Thoren <thoren.alex@gmail.com>
2023-05-08 17:44:31 +0200

    ✨ Added support for enumerating several images

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ def add_args():
     parser.add_argument("--extract", "-e", help="extract a specific feature or all features")
     parser.add_argument("--resize", "-rs", help="resize all images to a specific size", action="store_true")
     group = parser.add_mutually_exclusive_group()
-    group.add_argument("--random", "-r", help="select a random image from the dataset", action="store_true")
+    group.add_argument("--random", "-r", help="extract features from a random image in the dataset", action="store_true")
     group.add_argument("--image", "-i", help="image to extract features from. Format like 'PAT_86_1082_41.png'. The .png is optional", default=None)
+    group.add_argument("--images", "-is", help="number of images to extract features from, or 'all' for all images", default=None)
     args = parser.parse_args()
     return args
 
@@ -30,11 +31,15 @@ def clean_image(img: np.ndarray, to_binary: bool = False):
     return img
 
 def extract_features(args):
+    from feature_extraction.feature_extractor import FeatureExtractor
+    extractor = FeatureExtractor()
     # Prepare
     # Select resized images if they exist, else go with the original ones
     if os.path.exists("data/resized"):
+        path = "data/resized"
         imgs = os.listdir("data/resized")
     else:
+        path = "data/segmented"
         imgs = os.listdir("data/segmented")
 
     # Choose random image
@@ -47,37 +52,57 @@ def extract_features(args):
     # Choose user specified image
     if args.image is not None:
         imgs = [i for i in imgs if args.image in i]
+
+    def extract(img, mask):
+        # Check if "all" is specified
+        if args.extract == "all":
+            feats = extractor.do_all(img, mask)
+            return feats
+    
+        feat = extractor.extract_feat(args.extract, img, mask)
+        return feat
+
+    if args.images is not None:
+        non_masks = [i for i in imgs if "mask" not in i]
+        np.random.shuffle(non_masks)
+        if args.images.isdigit():
+            print(f"Using {args.images} randomly selected images")
+            non_masks = non_masks[:int(args.images)]
+
+        feats = []
+        for i, img_name in enumerate(non_masks):
+            print(f"{i+1}/{len(non_masks)}", end="\r")
+            img = plt.imread(f"{path}/{img_name}")
+            mask = plt.imread(f"{path}/{img_name[:-4]}_mask.png")
+            img = clean_image(img)
+            mask = clean_image(mask, to_binary=True)
+            result = extract(img, mask)
+            feats.append(result)
+        
+        return feats
+
     # Choose first image and corresponding mask
     img = [i for i in imgs if "mask" not in i][0]
     mask = img[:-4] + "_mask.png"
 
-    # Load image
-    img = plt.imread(f"data/segmented/{img}")
-    mask = plt.imread(f"data/segmented/{mask}")
-
-    # Clean image
+    # Load image and clean image
+    img = plt.imread(f"{path}/{img}")
+    mask = plt.imread(f"{path}/{mask}")
     img = clean_image(img)
     mask = clean_image(mask, to_binary=True)
     
-    from feature_extraction.feature_extractor import FeatureExtractor
-    extractor = FeatureExtractor()
-
-    # Check if "all" is specified
-    if args.extract == "all":
-        feats = extractor.do_all(img, mask)
-        return feats
-    
-    feat = extractor.extract_feat(args.extract, img, mask)
-    return feat
+    return extract(img, mask)
 
 def resize_images(size=(1024, 1024)):
     print("Resizing images...")
     from PIL import Image
     imgs = os.listdir("data/segmented")
-    for img_name in imgs:
+    for i, img_name in enumerate(imgs):
+        print(f"{i+1}/{len(imgs)}", end="\r")
         img = Image.open(f"data/segmented/{img_name}")
         img = img.resize(size)
         img.save(f"data/resized/{img_name}")
+    print("Done!")
 
 def main():
     args = add_args()
@@ -85,6 +110,7 @@ def main():
     if args.extract is not None:
         result = extract_features(args)
         print(result)
+
     
     if args.resize:
         resize_images()
