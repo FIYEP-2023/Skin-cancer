76bab92449484f7f04b087ce1b7e4b46e95bd831
Alexander Thoren <thoren.alex@gmail.com>
2023-06-01 20:02:39 +0200

    Added component and neighbor plots

--- model/figures.py
+++ model/figures.py
@@ -4,6 +4,12 @@ import os
 import matplotlib.pyplot as plt
 import skimage
 from scipy import ndimage
+import pickle
+from typing import Union, Tuple
+from model.topk import TopK
+import numpy.typing as npt
+from model.classifiers import KNN, Logistic, train_splits, evaluate_splits
+
 
 # Load csv
 df = pd.read_csv('data/metadata.csv')
@@ -149,7 +155,212 @@ def filter_histograms():
     plt.savefig('figures/filter_histograms.png', bbox_inches='tight')
 
 def optimisation_graphs():
-    pass
+    # COPIED FROM main.py
+    # this is so cringe holy shit literally copied the entirety of main just to create two fucking figures
+    def validate_file(filepath: str, prerequisite: Union[str, None] = None):
+        if not os.path.exists(filepath) or os.stat(filepath).st_size == 0:
+            raise FileNotFoundError(f"The file {filepath} does not exist or is empty. " + (f"Please run {prerequisite} first." if prerequisite is not None else ""))
+
+    def topk(top_k_k: Union[int, None] = None):
+        # Make sure our data exists
+        validate_file("data/features/X.pkl", "--extract all --images all")
+        validate_file("data/features/y.pkl", "--extract all --images all")
+        validate_file("data/features/img_names.pkl", "--extract all --images all")
+        validate_file("data/training/training_splits.pkl", "--split")
+
+        # Load data
+        with open("data/features/X.pkl", "rb") as f:
+            X: np.ndarray = np.array(pickle.load(f))
+        with open("data/features/y.pkl", "rb") as f:
+            y: np.ndarray = np.array(pickle.load(f))
+        with open("data/features/img_names.pkl", "rb") as f:
+            img_names: list[str] = pickle.load(f)
+        with open("data/training/training_splits.pkl", "rb") as f:
+            training_splits: list[npt.NDArray[np.str_]] = pickle.load(f)
+
+        # Set top_k_k to feature count if not specified
+        if top_k_k is None:
+            top_k_k = X.shape[1]
+
+        # Train topk on each training split
+        topks = []
+        for i, train in enumerate(training_splits):
+            # Get indices of images in this split
+            indices = [img_names.index(img) for img in train]
+            # Get features for this split
+            X_split = X[indices]
+            y_split = y[indices]
+            # Fit topk
+            topk = TopK(X_split, y_split, top_k_k)
+            topk.DO_ABCD_ONLY = False
+            topk.fit()
+            topks.append(topk)
+        
+        # Train topk on all training data
+        # Merge all training splits (remove duplicates)
+        train = np.concatenate(training_splits)
+        train = np.unique(train)
+        # Get indices of images in this split
+        indices = [img_names.index(img) for img in train]
+        # Get features for this split
+        X_split = X[indices]
+        y_split = y[indices]
+        # Fit topk
+        topk = TopK(X_split, y_split, top_k_k)
+        topk.fit()
+        
+        # Save to file
+        with open("data/training/full_pca.pkl", "wb") as f:
+            pickle.dump(topk, f)
+        with open("data/training/cross_val_pcas.pkl", "wb") as f:
+            pickle.dump(topks, f)
+
+        # Close loaded files
+        del X, y, img_names, training_splits
+
+    def train(k_neighbors: int = 5):
+        # Make sure our data exists
+        validate_file("data/features/X.pkl", "--extract all --images all")
+        validate_file("data/features/y.pkl", "--extract all --images all")
+        validate_file("data/features/img_names.pkl", "--extract all --images all")
+        validate_file("data/training/cross_val_pcas.pkl", "--pca")
+        validate_file("data/training/full_pca.pkl", "--pca")
+        validate_file("data/training/validation_splits.pkl", "--split")
+        validate_file("data/training/test_data.pkl", "--split")
+
+        # Load data
+        with open("data/features/X.pkl", "rb") as f:
+            X: np.ndarray = np.array(pickle.load(f))
+        with open("data/features/y.pkl", "rb") as f:
+            y: np.ndarray = np.array(pickle.load(f))
+        with open("data/features/img_names.pkl", "rb") as f:
+            img_names: list[str] = pickle.load(f)
+        with open("data/training/cross_val_pcas.pkl", "rb") as f:
+            topks: list[TopK] = pickle.load(f)
+        with open("data/training/full_pca.pkl", "rb") as f:
+            full_topk: TopK = pickle.load(f)
+        with open("data/training/validation_splits.pkl", "rb") as f:
+            validation_splits: list[npt.NDArray[np.str_]] = pickle.load(f)
+        with open("data/training/test_data.pkl", "rb") as f:
+            test_data: npt.NDArray[np.str_] = np.array(pickle.load(f))
+
+        # Process validation data
+        X_val_splits = []
+        y_val_splits = []
+        for i, split in enumerate(validation_splits):
+            # Get indices of images in this split
+            indices = [img_names.index(img) for img in split]
+            topk = topks[i]
+            # X
+            X_val = topk.transform(X[indices])
+            X_val_splits.append(X_val)
+            # y
+            y_val = y[indices]
+            y_val_splits.append(y_val)
+
+
+        # Train models on every split
+        models = train_splits(topks, X_val_splits, y_val_splits, n_neighbors=k_neighbors)
+
+        # Train full model on all training data
+        # Get indices of images in test data
+        indices = [img_names.index(img) for img in test_data]
+        # Get features for this split
+        X_test = full_topk.transform(X[indices])
+        y_test = y[indices]
+        # Fit KNN
+        knn = KNN(full_topk, X_test, y_test, n_neighbors=k_neighbors)
+        log = Logistic(full_topk, X_test, y_test)
+
+        # Save to file
+        with open("data/training/cross_val_models.pkl", "wb") as f:
+            pickle.dump(models, f)
+        with open("data/training/full_knn.pkl", "wb") as f:
+            pickle.dump(knn, f)
+        with open("data/training/full_log.pkl", "wb") as f:
+            pickle.dump(log, f)
+        
+        # Close loaded files
+        del X, y, img_names, topks, full_topk, validation_splits, test_data
+
+    def test_top_k(max_k: int, k_neighbors: int = 5):
+        """
+        Test different values of K for top K and plots K vs. f1 score.
+        """
+        k_neighbors = 5 if k_neighbors is None else k_neighbors
+        # Run tests
+        x = []
+        y = []
+        for n in range(1, max_k+1):
+            topk(n)
+            train(k_neighbors=k_neighbors)
+
+            # Load data
+            with open("data/training/cross_val_models.pkl", "rb") as f:
+                models: list[Tuple[KNN, Logistic]] = pickle.load(f)
+            
+            # Evaluate
+            stats = evaluate_splits(models)
+            f1 = stats["f1"][0] # [0] for knn
+            x.append(n)
+            y.append(f1)
+
+            # Close loaded files
+            del models
+        
+        # Plot results
+        fig, ax = plt.subplots()
+        ax.plot(x, y)
+        ax.set_xlabel("K")
+        ax.set_ylabel("F1 score")
+        ax.set_title("K vs. F1 score")
+
+        # Save x, y and plot to file
+        with open("data/training/components_x.pkl", "wb") as f:
+            pickle.dump(x, f)
+        with open("data/training/components_y.pkl", "wb") as f:
+            pickle.dump(y, f)
+        plt.savefig(f"figures/components_plot_{max_k}.png")
+
+    def test_neighbors(max_neighbors: int):
+        """
+        Test different numbers of neighbors for KNN and plots neighbors vs. errors.
+        """
+        # Run tests
+        x = []
+        y = []
+        for k in range(1, max_neighbors+1, 2): # Only odd numbers
+            train(k)
+
+            # Load data
+            with open("data/training/cross_val_models.pkl", "rb") as f:
+                models: list[Tuple[KNN, Logistic]] = pickle.load(f)
+            
+            # Evaluate
+            stats = evaluate_splits(models)
+            cf_knn, cf_log = stats["confusion_matrix"]
+            x.append(k)
+            y.append(cf_knn[1,0] + cf_knn[0,1])
+
+            # Close loaded files
+            del models
+        
+        # Create new plots
+        fig, ax = plt.subplots()
+        ax.plot(x, y)
+        ax.set_xlabel("Number of neighbors")
+        ax.set_ylabel("Errors")
+        ax.set_title("Number of neighbors vs. Errors")
+
+        # Save x, y and plot to file
+        with open("data/training/neighbors_x.pkl", "wb") as f:
+            pickle.dump(x, f)
+        with open("data/training/neighbors_y.pkl", "wb") as f:
+            pickle.dump(y, f)
+        plt.savefig(f"figures/neighbors_plot_{max_neighbors}.png")
+
+    test_neighbors(100)
+    test_top_k(50, k_neighbors=11)
 
 # Copied from feature_extractor.py
 def asymmetry_example(mask):
